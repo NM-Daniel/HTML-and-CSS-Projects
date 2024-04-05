a4be854a191f6b2daa9eeeecbd854b05787b52d9
NM-Daniel <165437058+NM-Daniel@users.noreply.github.com>
2024-04-05 15:40:14 -0700

    Basic_HTML_and_JavaScript_1 submission assignment
    Basic_HTML_1.html has the project requirements.

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+alert("Hello, World! This is my super cool alert");
+
+function My_First_Function() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	}
